8a5c935a87ea1efc5253b2d6d6f4db305491ecec
Michael Qiu <michaelqiu@MichaelMacBook-Pro-HOME.local>
2024-06-17 21:58:54 +0800

    init tauri 2.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    decentralizedtradedesk_lib::run()
 }
