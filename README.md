c2e444e0461bbae3af0ef49522ce10f22299a8fd
Michael Qiu <michaelqiu@MichaelMacBook-Pro-HOME.local>
2024-06-29 22:32:53 +0800

    docs(test formatting for tree-cli): tree

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ This template should help get you started developing with Tauri, React and Types
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
 
 ### How to output a tree diagram with module of tree
-$ pnpm install -g tree-cli
-$ npx tree -l 3 -o output.txt --ignore node_modules/
+- $ pnpm install -g tree-cli
+- $ npx tree -l 3 -o output.txt --ignore node_modules/
--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
     <div className='container'>
       <h1>Welcome to Tauri!</h1>
       <div className='row'>
-        <a href='https://vitejs.dev' target='_blank' rel="noreferrer">
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src='/vite.svg' className='logo vite' alt='Vite logo' />
         </a>
-        <a href='https://tauri.app' target='_blank' rel="noreferrer">
+        <a href='https://tauri.app' target='_blank' rel='noreferrer'>
           <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank' rel="noreferrer">
+        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
