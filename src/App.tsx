3ec0617c2fcb824e7965ec18183290995fc48bf4
Michael Qiu <michaelqiu@MichaelMacBook-Pro-HOME.local>
2024-06-29 22:02:00 +0800

    fix(semi of prettierrc): change semi to true

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { invoke } from '@tauri-apps/api/core'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import { invoke } from '@tauri-apps/api/core';
+import './App.css';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('')
-  const [name, setName] = useState('')
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name }))
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
     <div className='container'>
       <h1>Welcome to Tauri!</h1>
       <div className='row'>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel="noreferrer">
           <img src='/vite.svg' className='logo vite' alt='Vite logo' />
         </a>
-        <a href='https://tauri.app' target='_blank'>
+        <a href='https://tauri.app' target='_blank' rel="noreferrer">
           <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
+        <a href='https://reactjs.org' target='_blank' rel="noreferrer">
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
@@ -32,8 +32,8 @@ function App() {
       <form
         className='row'
         onSubmit={e => {
-          e.preventDefault()
-          greet()
+          e.preventDefault();
+          greet();
         }}
       >
         <input
@@ -46,7 +46,7 @@ function App() {
 
       <p>{greetMsg}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
